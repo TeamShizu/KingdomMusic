a472d0cd47e787cf35dab67efcd26ba65b87a2c8
TeamShizu <87710820+TeamShizu@users.noreply.github.com>
2021-11-25 08:36:56 +0530

    Update audioplay.py

--- KingdomMusic/audioplay.py
+++ KingdomMusic/audioplay.py
@@ -856,15 +856,7 @@ async def lol_cb(b, cb):
         appendable = [s_name, r_by, loc]
         qeue.append(appendable)
 
-        await callsmusic.pytgcalls.join_group_call(
-                chat_id, 
-                InputStream(
-                    InputAudioStream(
-                        file_path,
-                    ),
-                ),
-                stream_type=StreamType().local_stream,
-            )
+        await callsmusic.pytgcalls.join_group_call(chat_id,file_path)
         await cb.message.delete()
         await b.send_photo(chat_id,
             photo="final.png",
